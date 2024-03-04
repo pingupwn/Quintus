874b7eec6b3132bc238d6b99418000af4c91d574
Pragathiswaar AK <130224668+pingu0b@users.noreply.github.com>
2024-03-04 14:27:20 +0530

    Add files via upload

---
+++ src/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>Quintus</h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Welcome, <strong>{$username}</strong></span>
+      <img src={`https://api.dicebear.com/7.x/initials/svg?seed=${$username}`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+
+      <h3>Decentralised Chat</h3>
+  {/if}
+</header>
